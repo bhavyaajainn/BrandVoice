d54139bcbc1216e8371b7af2d475f8833aedf2c1
bhavyaajainn <103187367+bhavyaajainn@users.noreply.github.com>
2025-06-17 19:23:14 +0530

    solved merge errors

--- frontend/lib/store.ts
+++ frontend/lib/store.ts
@@ -1,14 +1,15 @@
 
+import { configureStore, ThunkDispatch, AnyAction } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import createSagaMiddleware from "redux-saga";
 import { authReducer } from "./redux/reducers/authReducer";
 import { brandReducer } from "./redux/reducers/brandReducer";
 import rootSaga from "./redux/sagas/rootSaga";
-import { configureStore } from "@reduxjs/toolkit";
-import createSagaMiddleware from "redux-saga";
-import userDataReducer from './slices/userslice'
-import userSchedulesReducer from './slices/userschedules'
-import createScheduleReducer from './slices/createschedule'
-import updateScheduleReducer from './slices/updateschedule'
-import deleteScheduleReducer from './slices/deleteschedule'
+import userDataReducer from './slices/userslice';
+import userSchedulesReducer from './slices/userschedules';
+import createScheduleReducer from './slices/createschedule';
+import updateScheduleReducer from './slices/updateschedule';
+import deleteScheduleReducer from './slices/deleteschedule';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -40,5 +41,8 @@ export const store = configureStore({
 sagaMiddleware.run(rootSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = ThunkDispatch<RootState, unknown, AnyAction>;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
